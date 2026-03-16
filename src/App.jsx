e2130832a5166a38a7980466100f6cd811693bb6
parvezpatwaryy <tanjilapatwaryy@gmail.com>
2026-03-16 11:42:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Countris from './assets/componance/countries/Countris'
+
+function App() {
+
+  return (
+    <>
+          <h1>React world on the go...</h1>
+          <Countris></Countris>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/componance/countries/Countris.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countris = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Countris;
