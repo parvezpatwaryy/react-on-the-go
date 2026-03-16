83fe9ac2f53dc973a1b999c4c3d2bf404594bc89
parvezpatwaryy <tanjilapatwaryy@gmail.com>
2026-03-16 18:33:26 +0600

    first commit

--- src/Countries/Countries.jsx
+++ src/Countries/Countries.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './countris.css'
-const Countries = ({counsry}) => {
-  console.log(counsry.continents.continents)
+const Countries = ({counsry,handleVisitedCountries}) => {
+  const  [visited , setVisited]= useState(false)
+  console.log(handleVisitedCountries)
+  // console.log(counsry.continents.continents)
+
+
+  const handleVisited=()=>{
+    if (visited){
+      setVisited(false)
+    }
+    else{
+      setVisited(true)
+    }
+  }
+
+    handleVisitedCountries(counsry)
   return (
-    <div className='country'>
+    <div className={`country ${visited && 'country-visited'}`}>
       <img src={counsry?.flags.flags.png} alt="counsry.flags.flags.alt" />
-      <h3>Name:{Countries.name.common}</h3>
+      <h3>Name:{Countries.name}</h3>
       <p>Population:{counsry.population.population}</p>
       <p>Capital:{counsry.capital.capital}</p>
       <p>Region{counsry.region.region}</p>
       <p>Languages:{counsry.languages.languages.eng}</p>
-      <h5>Area:{counsry.area.area}</h5>
+      <h5>Area:{counsry.area.area}{counsry.area.area >300000 ? "Big city":"small city"}</h5>
       <p>Continents:{counsry.continents.continents}</p>
+
+
+      <button onClick={handleVisited}>
+        {visited ? 'visited':'Not visited'}
+      </button>
     </div>
   );
 };
--- src/assets/componance/countries/Countris.jsx
+++ src/assets/componance/countries/Countris.jsx
@@ -1,17 +1,24 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import './Country.css'
 import Countries from '../../../Countries/Countries';
 const Countris = ({countrispromis}) => {
+  const [visitedCountris, setVisitedCountris] = useState([])
+
+  const handleVisitedCountries=(counsry)=>{
+    console.log('handle visited country',counsry)
+  }
   const countriesData = use(countrispromis);
   const countries = countriesData .countries;
   
   return (
     <div >
       <h1>In the counrtis:{countries.length}</h1>
+      <h3>Total country visited:</h3>
       <div className='country'>
         {
         countries.map(counsry=><Countries
           key={counsry.cca3.cca3}
+          handleVisitedCountries ={handleVisitedCountries}
           counsry={counsry}></Countries>)
       }
       </div>
